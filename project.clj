f69fb7d0e32f2235af42e6948256ede9f3f88924
bogdan demchenko <bogdandemchenko@gmail.com>
2020-08-12 23:01:24 +0300

    render html

--- project.clj
+++ project.clj
@@ -4,8 +4,10 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [
-    [org.clojure/clojure "1.10.1"]
-    [compojure "1.6.2"]
-    [ring/ring-core "1.8.1"]
-    [org.immutant/web "2.1.10"]]
+    [org.clojure/clojure        "1.10.1"]
+    [compojure                  "1.6.2"]
+    [ring/ring-core             "1.8.1"]
+    [org.immutant/web           "2.1.10"]
+    [rum                        "0.12.3"]
+    [org.clojure/clojurescript  "1.10.773"]]
   :repl-options {:init-ns whining.server})
--- src/whining/server.clj
+++ src/whining/server.clj
@@ -1,5 +1,6 @@
 (ns whining.server
   (:require
+    [rum.core :as rum]
     [immutant.web :as web]
     [compojure.core :as cj]
     [compojure.route :as cjr]))
@@ -15,13 +16,42 @@
     { :status 200
       :body (:uri req) }))
 
+(def posts
+  [{  :id       "123" 
+      :created  #inst "2020-01-01"
+      :author   "nikitonsky"
+      :body     "One of the most useless and most annoying UIs in iPhone. This window pop ups on a random phone in the room if you open the case (no, not only on the phones it has been paired to—literally on any phone in proximity).
+      But then it doesn’t reliably pop up when you actually need it (of course!). Sometimes I spend a few literal minutes opening and closing the case like a fool, putting airpods in or out, turning the phone on or off, in the hope it will finally show. This is stupid, because Airpods are connected via bluetooth to the phone already! I can listen for the audio, but can’t see the battery level, because, well, nobody knows why and there’s no button to press to force it to show.
+      Finally, look at the size of this thing! It covers the good half of the screen, and it’s the most important half (the bottom) where you are most likely to be doing something. Somehow after 13 years Apple was convinced that volume indicator shouldn’t cover the center of the screen. Next year we will see an incoming call notification that is, well, a tiny notification instead of a whole screen. Maybe eventually they’ll figure that out for airpods too? It only needs to show two numbers, come on!"}
+  {   :id       "456" 
+      :created  #inst "2020-01-03"
+      :author   "freetonik"
+      :body     "Simple rule: leave content where it is. Do not move it around.
+      Here I click on a particular dog image and immediately whole page gets relayouted, every image moves to another random place. It’s like I am looking at the whole new page"}])
+
+(rum/defc post [post]
+  [:.post
+    [:.post_sidebar
+      [:img.avatar {:src "/i/" (:author post) ".jpg"}]]
+    [:div
+      [:p [:span.author (:author post)] ": " (:body post)]
+      [:p.meta (:created post) "//" [:a (:href (str "/post/" (:id post)))]]]])
+
+(rum/defc index [posts]
+  [:html
+    [:body
+      (for [p posts]
+        (post p))]])
+
 (cj/defroutes routes
   (cj/GET "/" [:as req]
-    { :body "INDEX" })
+    { :body (rum/render-static-markup (index posts)) })
   (cj/GET "/write" [:as req]
     { :body "WRITE" })
   (cj/POST "/write" [:as req]
     { :body "POST" }))
+
+(def app routes)
 
 (defn -main [& args]
   (let [args-map (apply array-map args)
