c7f319dd64baa1236dd38dfe3240a8a07d3320d2
bogdan demchenko <bogdandemchenko@gmail.com>
2020-08-16 16:14:53 +0300

    encode foo

--- src/whining/server.clj
+++ src/whining/server.clj
@@ -38,6 +38,17 @@
   (.print date-formatter (DateTime. inst)))
 
 
+(defn encode-uri-component [s]
+  (-> s
+      (java.net.URLEncoder/encode "UTF-8")
+      (str/replace #"\+"    "%20")
+      (str/replace #"\%21"  "!")
+      (str/replace #"\%27"  "'")
+      (str/replace #"\%28"  "(")
+      (str/replace #"\%29"  ")")
+      (str/replace #"\%7E"  "-")))
+
+
 (defn send-mail! [{:keys [to subject body]}]
   (shell/sh
     "mail"
